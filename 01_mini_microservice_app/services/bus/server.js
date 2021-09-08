e21c58970fd8d5dcc8d1dcac9389dc5817da9f60
Jan Mathew <jan_mathew_calaunan@epam.com>
2021-09-08 14:10:46 +0800

    emmit events to moderation service

--- 01_mini_microservice_app/services/bus/server.js
+++ 01_mini_microservice_app/services/bus/server.js
@@ -6,14 +6,19 @@ app.use(express.json());
 
 app.post('/events', async (req, res) => {
   console.log('Event bus received event:', req.body);
+
   // Assume the whole request body contains the event
   const event = req.body;
 
   try {
+    console.log('1. Sending event to services...');
+
     await axios.post('http://localhost:4000/events', event);
     await axios.post('http://localhost:4010/events', event);
+    await axios.post('http://localhost:4015/events', event);
     await axios.post('http://localhost:4020/events', event);
 
+    console.log('2. Events sent!');
     res.status(200).send({ status: 'OK' });
   } catch (err) {
     res.send(err.message);
