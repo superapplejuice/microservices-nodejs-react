c5b49c36206792364e355155093a10f1f11d5c7e
Jan Mathew <jan_mathew_calaunan@epam.com>
2021-09-10 10:55:28 +0800

    filter comments by status

--- 01_mini_microservice_app/client/src/components/comment-list.js
+++ 01_mini_microservice_app/client/src/components/comment-list.js
@@ -1,16 +1,42 @@
 import React from 'react';
+import { APPROVED_STATUS, PENDING_STATUS, REJECTED_STATUS } from '../constants';
 
-const CommentList = ({ comments }) => (
-  <div>
-    <p>
-      {comments.length} comment{comments.length === 1 ? '' : 's'}
-    </p>
-    <ul>
-      {comments.map(comment => (
-        <li key={comment.id}>{comment.content}</li>
-      ))}
-    </ul>
-  </div>
-);
+const CommentList = ({ comments }) => {
+  const getFontStyleFromStatus = (status) => {
+    if (status === APPROVED_STATUS) {
+      return 'normal';
+    } else {
+      return 'italic';
+    }
+  };
+
+  const displayContentFromStatus = (status, content) => {
+    switch (status) {
+      case APPROVED_STATUS:
+        return content;
+      case REJECTED_STATUS:
+        return 'Comment is rejected';
+      case PENDING_STATUS:
+        return 'Comment is awaiting moderation';
+      default:
+        return '';
+    }
+  };
+
+  return (
+    <div>
+      <p>
+        {comments.length} comment{comments.length === 1 ? '' : 's'}
+      </p>
+      <ul>
+        {comments.map(({ id, content, status }) => (
+          <li key={id} style={{ fontStyle: getFontStyleFromStatus(status) }}>
+            {displayContentFromStatus(status, content)}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default CommentList;
--- 01_mini_microservice_app/client/src/constants.js
+++ 01_mini_microservice_app/client/src/constants.js
@@ -1,3 +1,7 @@
 export const COMMENTS_URL = 'http://localhost:4010/posts/:id/comments';
 export const POSTS_URL = 'http://localhost:4000/posts';
 export const QUERY_URL = 'http://localhost:4020/posts';
+
+export const PENDING_STATUS = 'PENDING';
+export const APPROVED_STATUS = 'APPROVED';
+export const REJECTED_STATUS = 'REJECTED';
